e753bb06f0b43bcc9e65aedbc248e410a743ba13
chhayphinavuth <chhayphinavuth@acledaaib.onmicrosoft.com>
2026-06-11 15:44:24 +0700

    New Form 11

--- Form10.Designer.cs
+++ Form10.Designer.cs
@@ -28,18 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            uiTabControlMenu1 = new Sunny.UI.UITabControlMenu();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            uiTabControlMenu1.SuspendLayout();
             SuspendLayout();
+            // 
+            // uiTabControlMenu1
+            // 
+            uiTabControlMenu1.Alignment = TabAlignment.Left;
+            uiTabControlMenu1.Controls.Add(tabPage1);
+            uiTabControlMenu1.Controls.Add(tabPage2);
+            uiTabControlMenu1.DrawMode = TabDrawMode.OwnerDrawFixed;
+            uiTabControlMenu1.Font = new Font("Microsoft Sans Serif", 12F);
+            uiTabControlMenu1.Location = new Point(98, 43);
+            uiTabControlMenu1.Multiline = true;
+            uiTabControlMenu1.Name = "uiTabControlMenu1";
+            uiTabControlMenu1.SelectedIndex = 0;
+            uiTabControlMenu1.Size = new Size(587, 313);
+            uiTabControlMenu1.SizeMode = TabSizeMode.Fixed;
+            uiTabControlMenu1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(201, 0);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Size = new Size(386, 313);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(201, 0);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Size = new Size(0, 172);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form10
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(uiTabControlMenu1);
             Name = "Form10";
             Text = "Using Method";
+            uiTabControlMenu1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Sunny.UI.UITabControlMenu uiTabControlMenu1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
